5b013a3a65aa1dc761cb7d0b0009644a2fc12741
DarxyDev <damongarvin@gmail.com>
2023-03-01 19:11:50 -0500

    refactored ui-manager.js

--- src/ui-manager.js
+++ src/ui-manager.js
@@ -12,13 +12,10 @@ const uiManager = (() => {
             openNewTaskMenu: document.getElementById('open-new-task-menu-btn'),
             addCategory: document.getElementById('create-new-category-btn'),
         },
-        categories: [],
-        items: [],
-        main: document.getElementById('main'),
-        menu:{
+        menu: {
             newTask: {
                 container: document.getElementById('add-task-popup'),
-                button:{
+                button: {
                     submit: document.getElementById('create-task-btn'),
                 },
                 form: {
@@ -27,21 +24,65 @@ const uiManager = (() => {
                     description: document.getElementById('new-task-form-description'),
                 },
             },
-        }
+        },
+        //dynamic references
+        categories: [], 
+        items: [],
     }
     //variables
     let selectedCategoryIndex = 0;
-    //functions
 
+    //Initialize - Call in index.js
+    const init = () => {
+        _selectCategory(selectedCategoryIndex);
+
+        //set event listeners
+        ref.button.addCategory.addEventListener('click', _addNewCategory);
+        function _addNewCategory() {
+            console.log('Add category button hit.');
+        }
+        ref.button.openNewTaskMenu.addEventListener('click', openAddTaskMenu);
+        function openAddTaskMenu() {
+            _showBlackout(true);
+            _showAddTaskMenu(true);
+        }
+        ref.menu.newTask.button.submit.addEventListener('click', () => {
+            let formElements = ref.menu.newTask.form;
+            let name = formElements.name.value;
+            let description = formElements.description.value;
+
+            let category = mainManager.getCategoryArray()[selectedCategoryIndex];
+            let task = category.newTask(name, description);
+            addTasks(task);
+
+            _showBlackout(false);
+            _showAddTaskMenu(false);
+            _resetTaskForm();
+        })
+    }
+
+    //public functions
     const addCategories = (categories) => {
-        if (!Array.isArray(categories)) {
-            let categoryElement = _createCategory(categories);
-            ref.container.category.appendChild(categoryElement);
+        if (Array.isArray(categories)) {
+            categories.forEach(category => { addCategories(category) });
             return;
         }
-        categories.forEach(category => { addCategories(category) });
-
+        let categoryElement = _createCategory(categories);
+        ref.container.category.appendChild(categoryElement);
     }
+    const addTasks = (tasks) => {
+        if (Array.isArray(tasks)) {
+            tasks.forEach(task => { addTasks(task) });
+            return;
+        }
+        let item = _createItem(tasks);
+        ref.items.push(item);
+        ref.container.item.appendChild(item);
+    }
+    const getSelectedCategoryIndex = () => { return selectedCategoryIndex; }
+
+
+    //private functions
     const _createCategory = (category) => {
         let categoryElement = document.createElement('category');
 
@@ -57,16 +98,6 @@ const uiManager = (() => {
         ref.categories.push(categoryElement);
 
         return categoryElement;
-    }
-
-    const addTasks = (tasks) => {
-        if (!Array.isArray(tasks)) {
-            let item = _createItem(tasks);
-            ref.items.push(item);
-            ref.container.item.appendChild(item);
-            return;
-        }
-        tasks.forEach(task => { addTasks(task) });
     }
     const _createItem = (task) => {
         let item = document.createElement('item');
@@ -95,55 +126,19 @@ const uiManager = (() => {
         ref.items = [];
         ref.container.item.textContent = '';
     }
-    const _clearCategories = () => {
-        ref.categories = [];
-        ref.container.category.textContent = [];
+    function _resetTaskForm() {
+        ref.menu.newTask.form.form.reset();
     }
-    const getSelectedCategoryIndex = () => {
-        return selectedCategoryIndex;
+    const _showBlackout = (bool) => {
+        if (bool) ref.container.blackout.classList.remove('hidden');
+        else ref.container.blackout.classList.add('hidden');
     }
-    const init = () => {
-        _selectCategory(selectedCategoryIndex);
-        
-        //event listeners
-        ref.button.addCategory.addEventListener('click', _addNewCategory);
-        function _addNewCategory() {
-            console.log('Add category button hit.');
-        }
-        ref.button.openNewTaskMenu.addEventListener('click', openAddTaskMenu);
-        function openAddTaskMenu() {
-            _showBlackout(true);
-            _showAddTaskMenu(true);
-        }
-        function closeAddTaskMenu(){
-            _showBlackout(false);
-            _showAddTaskMenu(false);
-        }
-        const _showBlackout = (displayBlackout) => {
-            if (displayBlackout) ref.container.blackout.classList.remove('hidden');
-            else ref.container.blackout.classList.add('hidden');
-        }
-        const _showAddTaskMenu = (displayAddTaskMenu) =>{
-            if(_showAddTaskMenu) ref.menu.newTask.container.classList.remove('hidden');
-            else ref.menu.newTask.container.classList.add('hidden');
-        }
-        ref.menu.newTask.button.submit.addEventListener('click', _createTask);
-        function _createTask(){
-            let formElements = ref.menu.newTask.form;
-            let name = formElements.name.value;
-            let description = formElements.description.value;
+    const _showAddTaskMenu = (bool) => {
+        if (bool) ref.menu.newTask.container.classList.remove('hidden');
+        else ref.menu.newTask.container.classList.add('hidden');
+    }
 
-            let category = mainManager.getCategoryArray()[selectedCategoryIndex];
-            let task = category.newTask(name, description);
-            addTasks(task);
-            
-            closeAddTaskMenu();
-            _clearNewTaskForm();
-        }
-        function _clearNewTaskForm(){
-            ref.menu.newTask.form.form.reset();
-        }
-    }
+
     return { ref, addTasks, addCategories, getSelectedCategoryIndex, init };
 })()
 export default uiManager;
